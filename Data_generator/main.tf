4dbeb16aeba3129461c87ccfc41ec64d247688a8
Marcanta7 <marcanta7@gmail.com>
2025-02-13 13:17:21 +0100

    Prueba TF

--- Data_generator/main.tf
+++ Data_generator/main.tf
@@ -6,27 +6,105 @@ resource "google_artifact_registry_repository" "repo" {
   format        = "DOCKER"
 }
 
-resource "google_cloudbuild_trigger" "build_trigger" {
-  name     = "github-build-trigger"
-  location = var.region
-  project  = var.project_id
 
-  github {
-    owner = "Marcanta7"
-    name  = "Prueba"
-    push {
-      branch = "main"  # O la rama donde esté el código
-    }
+
+
+
+resource "google_cloudbuild_trigger" "build_trigger" {
+  name     = "build-trigger"
+  location = "global"
+
+  # Configuración del repositorio y rama
+  trigger_template {
+    branch_name = "main"
+    repo_name   = "Prueba"
   }
+
+  # Información de la cuenta de servicio para ejecutar el trigger
+  service_account = google_service_account.cloudbuild_service_account.id
+
+  # Archivo de configuración de Cloud Build
+  filename = "cloudbuild.yaml"
 
   build {
+    # Definir los pasos del build
     step {
-      name = "gcr.io/cloud-builders/docker"
-      args = ["build", "-t", "europe-west1-docker.pkg.dev/${var.project_id}/volunteer-matching-repo/generator:latest", "."]
+      name   = "gcr.io/cloud-builders/gsutil"
+      args   = ["cp", "gs://mybucket/remotefile.zip", "localfile.zip"]
+      timeout = "120s"
+      secret_env = ["MY_SECRET"]
     }
-    images = ["europe-west1-docker.pkg.dev/${var.project_id}/volunteer-matching-repo/generator:latest"]
+
+    step {
+      name   = "ubuntu"
+      script = "echo hello"
+    }
+
+    # Fuentes de los archivos para el build
+    source {
+      storage_source {
+        bucket = "mybucket"
+        object = "source_code.tar.gz"
+      }
+    }
+
+    # Etiquetas y variables de sustitución
+    tags = ["build", "newFeature"]
+    substitutions = {
+      _FOO = "bar"
+      _BAZ = "qux"
+    }
+
+    queue_ttl = "20s"
+    logs_bucket = "gs://mybucket/logs"
+
+    secret {
+      kms_key_name = "projects/myProject/locations/global/keyRings/keyring-name/cryptoKeys/key-name"
+      secret_env = {
+        PASSWORD = "ZW5jcnlwdGVkLXBhc3N3b3JkCg=="
+      }
+    }
+
+    available_secrets {
+      secret_manager {
+        env          = "MY_SECRET"
+        version_name = "projects/myProject/secrets/mySecret/versions/latest"
+      }
+    }
+
+    artifacts {
+      images = ["gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA"]
+      objects {
+        location = "gs://bucket/path/to/somewhere/"
+        paths = ["path"]
+      }
+    }
   }
+
+  # Permitir logs de build
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
 }
+
+# Crear la cuenta de servicio para Cloud Build
+resource "google_service_account" "cloudbuild_service_account" {
+  account_id = "cloud-sa"
+}
+
+# Asignar el rol de servicio para la cuenta de servicio de Cloud Build
+resource "google_project_iam_member" "act_as" {
+  project = data.google_project.project.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
+# Asignar el rol de escritor de logs a la cuenta de servicio
+resource "google_project_iam_member" "logs_writer" {
+  project = data.google_project.project.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
+
 
 resource "google_pubsub_topic" "volunteers" {
   name    = "volunteers"
@@ -37,6 +115,10 @@ resource "google_pubsub_topic" "affected" {
   name    = "affected"
   project = var.project_id
 }
+
+
+
+
 
 resource "google_cloud_run_service" "generator" {
   name     = "volunteer-generator"
@@ -54,8 +136,15 @@ resource "google_cloud_run_service" "generator" {
       }
     }
   }
-  depends_on = [google_artifact_registry_repository.repo]
+
+  depends_on = [
+    google_artifact_registry_repository.repo,
+    google_cloudbuild_trigger.build_trigger
+  ]
 }
+
+
+
 
 variable "project_id" {
   description = "El ID del proyecto"
